ff869d5f710fa1dde3228d2835e3650b23d6e4db
acmendoza11 <ailger.cmendoza@gmail.com>
2022-04-15 00:59:18 +0800

    expo-av used to stream

--- react-native-first/components/screens/Camera.js
+++ react-native-first/components/screens/Camera.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
 import Header from '../Header';
+import { StyleSheet, View } from 'react-native';
+import Constants from 'expo-constants';
 
 const Camera = () => {
+
   return (
-    <View>
-      <Header title="Camera" />
-    </View>
+    
+      
+      <WebView 
+        style={styles.container}
+        source={{ uri: 'http://192.168.1.5:8080/?action=stream' }}
+      />
+    
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
+  },
+});
+
 
 export default Camera;
